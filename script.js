407b4dd9997354fe48f194cee23556b8b535e67d
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-05-13 09:27:26 -0400

    grab and gen work, make content later

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
+"use strict";
 class Player {
   fname;
   lname;
   age;
   ally;
   job;
-  constructor(fname, lname, age, ally, job) {
+  img;
+  bonus;
+  constructor(fname, lname, age, ally, job, img, bonus) {
     (this.fname = fname),
       (this.lname = lname),
       (this.age = age),
       (this.ally = ally),
-      (this.job = job);
+      (this.job = job),
+      (this.img = img),
+      (this.bonus = bonus);
   }
-  print() {
-    console.log(this);
+  render(loc, num) {
+    const card = DOC.create(
+      "div",
+      `CARD${num}`,
+      `${this.ally[0]}`,
+      "col-12",
+      "col-sm-6",
+      "col-md-4"
+    );
+    const content = DOC.create("div");
+    content.textContent = `${this.fname} ${this.lname}`;
+    card.setAttribute("data-name", `c${this.fname}&${this.lname}`);
+    card.append(content);
+    loc.append(card);
   }
 }
-
 const Team = {
-  players: undefined,
-  setup: function () {},
+  players: [],
+  setup: function () {
+    fetch("team.txt")
+      .then((result) => result.text())
+      .then((data) => {
+        data.split(";").forEach((e) => {
+          this.players.push(new Player(...e.split(",")));
+        });
+        this.players.forEach((p, i) => p.render(DOC.get("#gridRow"), i));
+      }) //renders and assigns
+      .catch((error) => {
+        throw new Error(error);
+      });
+  },
 };
+const DOC = {
+  e: document,
+  body: document.body,
+  get: function (arg) {
+    return this.e.querySelector(arg);
+  },
+  getALL: function (arg) {
+    return Array.from(this.e.querySelectorAll(arg));
+  },
+  create: function (tag, id = "", ...classes) {
+    let e = this.e.createElement(tag);
+    e.id = id;
+    if (classes.length > 0) e.classList.add(...classes);
+    return e;
+  },
+};
+Team.setup();
